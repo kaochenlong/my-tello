04327f80399003b5d5eb973bbc76e00af406cb6d
Eddie Kao <eddie@5xruby.tw>
2020-02-12 06:56:21 +0800

    15 - 新增卡片 part 1

--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -3,18 +3,53 @@
     <h2 class="header">{{ list.name }}</h2>
 
     <div class="deck">
-      <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+      <Card v-for="card in cards" :card="card" :key="card.id"></Card>
+
+      <div class="input-area">
+        <textarea class="content" v-model="content"></textarea>
+        <button class="button" @click="createCard">新增卡片</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from '@rails/ujs';
 import Card from 'components/card';
 
 export default {
   name: 'List', 
   props: ["list"], 
-  components: { Card }
+  components: { Card }, 
+  data: function() {
+    return {
+      content: '', 
+      cards: this.list.cards
+    }
+  }, 
+  methods: {
+    createCard(event) {
+      event.preventDefault();
+
+      let data = new FormData();
+      data.append("card[list_id]", this.list.id);
+      data.append("card[name]", this.content);
+
+      Rails.ajax({
+        url: '/cards', 
+        type: 'POST', 
+        data, 
+        dataType: 'json', 
+        success: resp => {
+          this.cards.push(resp);
+          this.content = "";
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    }
+  }
 }
 </script>
 
@@ -28,6 +63,27 @@ export default {
 
   .deck {
     @apply .mt-2;
+  }
+
+
+  .input-area {
+    @apply .mt-2;
+
+    .content {
+      @apply .w-full .p-2 .rounded-sm;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
+
+    .button {
+      @apply .px-3 .py-1 .font-semibold .text-sm .bg-blue-300 .rounded;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
   }
 }
 </style>
