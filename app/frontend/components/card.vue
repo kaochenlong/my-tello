13ab51d9221d9b62d705529032facdedf75f54b4
Eddie Kao <eddie@5xruby.tw>
2020-02-12 06:34:00 +0800

    14 - 元件化 part 1 (Card)

---
+++ app/frontend/components/card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    {{ card.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card', 
+  props: ['card']
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  @apply .bg-gray-100 .mb-2 .rounded-sm .px-2 .py-2;
+}
+</style>
--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="list">
     <h2 class="header">{{ list.name }}</h2>
+
+    <div class="deck">
+      <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from 'components/card';
+
 export default {
   name: 'List', 
-  props: ["list"]
+  props: ["list"], 
+  components: { Card }
 }
 </script>
 
 <style lang="scss" scoped>
 .list {
-  @apply .bg-gray-300 .mx-2 .w-64 .rounded;
+  @apply .bg-gray-300 .mx-2 .w-64 .rounded .px-3 .py-1 ;
 
   .header {
-    @apply .px-3 .py-1 .font-bold;
+    @apply .font-bold;
+  }
+
+  .deck {
+    @apply .mt-2;
   }
 }
 </style>
