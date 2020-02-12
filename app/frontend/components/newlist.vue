be0db32e3d982ee9fcebd7ac3ab965f613ebbbb4
Eddie Kao <eddie@5xruby.tw>
2020-02-12 17:49:04 +0800

    24 - 新增列表 part 1

---
+++ app/frontend/components/newlist.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <button v-if="!creatingList" class="new_button" @click="newList">
+      <i class="fas fa-plus"></i> 新增其它列表
+    </button>
+    <input v-if="creatingList" type="text" class="list_name" placeholder="列表標題">
+    <button v-if="creatingList" @click="createList" class="button create_button">建立</button>
+    <button v-if="creatingList" @click="creatingList = false" class="button cancel_button">取消</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Newlist', 
+  data: function() {
+    return {
+      creatingList: false
+    }
+  }, 
+  methods: {
+    newList(event) {
+      event.preventDefault();
+      this.creatingList = true;
+    }, 
+
+    createList(event) {
+      event.preventDefault();
+      console.log('create!');
+      this.creatingList = false;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.new_button {
+  @apply .font-bold .w-full .text-left;
+
+  &:focus {
+    @apply .outline-none;
+  }
+}
+
+.list_name {
+  @apply .rounded .w-full .px-3 .py-2 .mb-2;
+
+  &:focus {
+    @apply .outline-none;
+  }
+}
+
+.button {
+  @apply .px-3 .py-1 .rounded .font-bold .text-sm;
+
+  &:focus {
+    @apply .outline-none;
+  }
+}
+
+.create_button {
+  @apply .bg-gray-400;
+}
+
+.cancel_button {
+  @apply .bg-white;
+}
+
+.container {
+  @apply .bg-gray-300 .px-3 .py-2 .mx-2 .w-64 .h-full .rounded .flex-none;
+}
+</style>
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -12,11 +12,12 @@ import 'styles'
 // vue.js
 
 import Vue from 'vue/dist/vue.esm';
-import List from 'components/list';
 import Rails from '@rails/ujs';
 import draggable from 'vuedraggable';
 import store from 'stores/list';
 import { mapGetters, mapActions } from 'vuex';
+import List from 'components/list';
+import Newlist from 'components/newlist';
 
 document.addEventListener("turbolinks:load", function(event) {
   let el = document.querySelector('#board');
@@ -37,7 +38,7 @@ document.addEventListener("turbolinks:load", function(event) {
           }
         }
       },
-      components: { List, draggable }, 
+      components: { List, Newlist, draggable }, 
       methods: {
         ...mapActions(["loadList", "moveList"]), 
       }, 
